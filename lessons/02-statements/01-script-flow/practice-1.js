2a98a3550477ef3cbc365b9730fb50c88c3b44ae
Daniel Farr <dfarr@logitech.com>
2023-01-24 15:43:50 -0700

    script flow practice

---
+++ lessons/02-statements/01-script-flow/practice-1.js
@@ -0,0 +1,23 @@
+/**
+ * Challenge
+ * Fix the code so that it runs correctly.
+ */
+
+const firstName1 = 'daniel';
+const firstName2 = 'kate';
+const firstName3 = 'cassidy';
+
+const makeFullName = function(firstName lastName){
+  fullName = firstName + ' ' + lastName;
+
+  return fulllllname
+}
+
+const listOfNames = [fullName1, fullName2, fullName3];
+
+console.log(listOfNames);
+
+const fullName1 = makeFullName(firstName1, lastNameAll);
+const fullName2 = makeFullName(firstName2, lastNameAll);
+const fullName3 = makeFullName(firstName3, lastNameAll);
+
